0abd337c721bc99052dadda9d0f29219079e3229
Hugo L. Ruffo <hlruffo@gmail.com>
2023-03-23 20:09:27 -0300

    form completed

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -18,10 +18,10 @@ from django.urls import path, include
 from core.views import index, contact
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', index, name='index'),
-    path('items/', include('item.urls')),
-    path('contact/', contact, name='contact'),
+    path('',include('core.urls')),
+    path('items/', include('item.urls')),    
     path('admin/', admin.site.urls),
     
 ]+ static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
